fe7e55906f74d68b82c56a309661f7d5b7ba9a71
PabloVallecillos <pablovallecillosmoya@gmail.com>
2020-09-25 11:41:52 +0200

    initialCommit

---
+++ app.js
@@ -0,0 +1,102 @@
+const path = require('path');
+const connectDB = require('./config/db');
+const dotenv = require('dotenv');
+const express = require('express');
+const morgan = require('morgan');
+const passport = require('passport');
+const methodOverride = require('method-override')
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+const mongoose = require('mongoose');
+const MongoStore = require('connect-mongo')(session);
+
+// Load config
+dotenv.config({ path: './config/config.env' });
+
+// Passport config
+require('./config/passport')(passport);
+
+connectDB();
+
+const app = express();
+
+// Body parser
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Method - override
+app.use(methodOverride(function (req, res) {
+  if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+    // look in urlencoded POST bodies and delete it
+    let method = req.body._method
+    delete req.body._method
+    return method
+  }
+}))
+
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+// Session
+app.use(
+  session({
+    secret: 'keyboard cat',
+    resave: false, // dont save the session if doesn´t modify
+    saveUninitialized: false, // don´t create the session until something is stored
+    // cookie: { secure: true }   https
+    store: new MongoStore({ mongooseConnection: mongoose.connection }),
+  })
+);
+
+// Handlebars Helpers
+const {
+  formatDate,
+  stripTags,
+  truncate,
+  editIcon,
+  select,
+} = require('./helpers/hbs');
+
+//Handlebars
+app.engine(
+  '.hbs',
+  exphbs({
+    helpers: {
+      formatDate,
+      stripTags,
+      truncate,
+      editIcon,
+      select,
+    },
+    defaultLayout: 'main',
+    extname: '.hbs',
+  })
+);
+app.set('view engine', '.hbs');
+
+// Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Set global var
+app.use((req, res, next) => {
+  res.locals.user = req.user || null;
+  next();
+});
+
+// Static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Routes
+app.use('/', require('./routes/index'));
+app.use('/auth', require('./routes/auth'));
+app.use('/stories', require('./routes/stories'));
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(
+  PORT,
+  console.log(`Server runing in ${process.env.NODE_ENV} mode on port ${PORT}`)
+);
---
+++ public/js/style.js
@@ -0,0 +1,19 @@
+// const hamburguer = document.getElementsByClassName('sidenav-trigger')[0];
+// const sidenavUl = document.getElementById('mobile-demo');
+// const container = document.getElementsByClassName('container')[1];
+// const div = document.createElement('div');
+
+// hamburguer.addEventListener('click', () => {
+//   sidenavUl.classList.toggle('mystyle');
+//   div.classList.toggle('blacky');
+
+//   setTimeout(() => {
+//     document.body.appendChild(div);
+//     div.style.opacity = '0.3';
+//   }, 500);
+
+//   if (!sidenavUl.classList.contains('mystyle')) {
+//     document.body.removeChild(div);
+//   }
+
+// });
